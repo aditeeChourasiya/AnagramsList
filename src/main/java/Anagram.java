eed613d82bac571597b8986eafea5fd02c80e84d
aditeechourasiya <aditee.chourasiya@gmail.com>
2023-05-28 10:22:41 +0200

    ANAGRAMlIST

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Set;
+
+public class Anagram
+{
+     public static void main(String[] args) throws IOException {
+        // list of words
+         List<String> words = Files.readAllLines(new File("C:/Users/bhand/Java Projects/AnagramsList/src/main/resources/words-utf8.txt").toPath(), StandardCharsets.UTF_8 );
+
+         // remove blank lines
+         words.removeAll(List.of(""));
+
+        if(words.size()!= 0){
+            Set<Set<String>> anagrams = AnagramFinder.groupAnagrams(words);
+            anagrams.forEach(anagram -> System.out.println(anagram));
+        }else {
+            System.out.println("File is empty");
+        }
+    }
+}
