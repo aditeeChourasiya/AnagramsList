2b8123bf27b3f90a0fd6888fc316adda00c69d22
aditeechourasiya <aditee.chourasiya@gmail.com>
2023-05-29 14:06:20 +0200

    Group ANAGRAMS

--- src/main/java/AnagramFinder.java
+++ src/main/java/AnagramFinder.java
@@ -2,41 +2,39 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class AnagramFinder {
-    // Function to group anagrams from a given list of words
+public class AnagramFinder {public static Set<Set<String>> groupAnagrams(List<String> words)
+{
+    // a set to store anagrams
+    Set<Set<String>> anagrams = new HashSet<>();
 
-    public static Set<Set<String>> groupAnagrams(List<String> words)
+    // sort each word on the list
+    List<String> list = words.stream()
+            .map(s -> Stream.of(s.split("")).sorted()
+                    .collect(Collectors.joining()))
+            .collect(Collectors.toList());
+
+    // construct a map where the key is each sorted word,
+    // and value is a list of indices where it is present
+    Map<String, List<Integer>> map = new HashMap<>();
+    for (int i = 0; i < list.size(); i++)
     {
-        // a set to store anagrams
-        Set<Set<String>> anagrams = new HashSet<>();
-
-        // sort each word on the list
-        List<String> list = words.stream()
-                .map(s -> Stream.of(s.split("")).sorted()
-                        .collect(Collectors.joining()))
-                .collect(Collectors.toList());
-
-        // construct a map where the key is each sorted word,
-        // and value is a list of indices where it is present
-        Map<String, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < list.size(); i++)
-        {
-            String n = list.get(i);
-            map.putIfAbsent(n, new ArrayList<>());
-            map.get(n).add(i);
-        }
-
-        // traverse the map and read indices for each sorted key.
-        for (Map.Entry<String, List<Integer>> entry: map.entrySet())
-        {
-            //Collect words for the indices for each sorted key from input text file
-            Set<String> collection = entry.getValue().stream()
-                    .map(index -> words.get(index))
-                    .collect(Collectors.toSet());
-            if (collection.size() > 1) {
-                anagrams.add(collection);
-            }
-        }
-       return anagrams;
+        String n = list.get(i);
+        map.putIfAbsent(n, new ArrayList<>());
+        map.get(n).add(i);
     }
+
+    // traverse the map and read indices for each sorted key.
+    for (Map.Entry<String, List<Integer>> entry: map.entrySet())
+    {
+        //Collect words for the indices for each sorted key from input text file
+        Set<String> collection = entry.getValue().stream()
+                .map(index -> words.get(index))
+                .collect(Collectors.toSet());
+        if (collection.size() > 1) {
+            anagrams.add(collection);
+        }
+    }
+
+    return anagrams;
+}
 }
