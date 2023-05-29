b40846a7fd3bf02975e4f1a01f1fce9b54dc6d04
aditeechourasiya <aditee.chourasiya@gmail.com>
2023-05-29 14:05:36 +0200

    ANAGRAMlIST

--- src/main/java/Anagram.java
+++ src/main/java/Anagram.java
@@ -7,15 +7,16 @@ import java.util.Set;
 
 public class Anagram
 {
-     public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
         // list of words
-         List<String> words = Files.readAllLines(new File("C:/Users/bhand/Java Projects/AnagramsList/src/main/resources/words-utf8.txt").toPath(), StandardCharsets.UTF_8 );
+        List<String> words = Files.readAllLines(new File("C:/Users/bhand/Java Projects/AnagramsList/src/main/resources/words-utf8.txt").toPath(), StandardCharsets.UTF_8 );
 
-         // remove blank lines
-         words.removeAll(List.of(""));
+        // remove blank lines
+        words.removeAll(List.of(""));
 
         if(words.size()!= 0){
             Set<Set<String>> anagrams = AnagramFinder.groupAnagrams(words);
+            System.out.println("List of anagrams : ");
             anagrams.forEach(anagram -> System.out.println(anagram));
         }else {
             System.out.println("File is empty");
